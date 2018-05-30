0ae38b849bf7dee00ffb633a1649cbf33b404fdd
Ahshan Zishan <dantex696@outlook.com>
2018-05-30 04:17:50 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 /* Task 1 */
-
+var heading = document.getElementById('heading')
+console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
-
-
+//Task 2a
+var textBtn=document.getElementById('textbtn2')
+textBtn.addEventListener('click', function(){
+    var task2=document.getElementById('textbtn2')
+    var paragraph= document.createElement('p')
+    paragraph.innerText="hello World"
+    task2.appendChild(paragraph)
+})
 
 /* Task 4 using jQuery */
